ec2310dad24d36376719b27e65089f0696683bfd
Jyun-yu Chen <jyun.yu.chen.79@gmail.com>
2023-04-29 22:13:26 +0800

    練習

---
+++ video_download.js
@@ -0,0 +1,19 @@
+
+let fileName = '';
+function btnClick(){
+    fetch("http://localhost:8080/video-download")
+    .then(response => {
+        // 獲取headers
+        const headers = response.headers;
+        const contentDisposition = headers.get('Content-Disposition');
+        fileName = contentDisposition.split(';');
+        fileName = decodeURIComponent(fileName[2].split('=')[1].replace(/"/g, ''));
+        console.log(fileName);
+        return response.blob()
+    })
+    .then(blob =>{
+        const file = new File([blob], fileName);
+        saveAs(file);
+    });
+
+}
